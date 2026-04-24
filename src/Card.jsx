a79289e8ec576e829d95c6d7bd7aa8b191c918a7
ayoubgz1 <ayoub.guendouz13@gmail.com>
2026-04-24 23:34:05 +0100

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,7 +9,7 @@ function Card({salat,time,setCursorText}) {
  
   return (
   <>
-      <div onMouseEnter={()=>{setCursorText(time+" "+salat)}}  onMouseLeave={HnadleMouseLeave}  className=" card sticky top-[350px] bg-[url(public/HR-18.jpg)]
+      <div onMouseEnter={()=>{setCursorText(time+" "+salat)}}  onMouseLeave={HnadleMouseLeave}  className=" card sticky top-[350px] bg-[url(/HR-18.jpg)]
         bg-cover bg-center bg-no-repeat w-[320px] h-[250px] object-cover flex justify-center
          items-end flex-col rounded-2xl shadow-2xl shadow-amber-50/[0.3]
          p-8 transition-all sm:w-[720px] z-40
